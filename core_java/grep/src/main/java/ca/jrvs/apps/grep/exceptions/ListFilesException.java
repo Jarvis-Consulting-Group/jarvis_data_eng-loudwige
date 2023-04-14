4d4db5ff706b36a6dcab6645c164332a9d2ff499
loudwige <55907221+loudwige@users.noreply.github.com>
2023-04-14 10:53:19 +0800

    Revert "Java Grep App implementation without Stream API"

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/exceptions/ListFilesException.java
+++
@@ -1,9 +0,0 @@
-package ca.jrvs.apps.grep.exceptions;
-
-import java.io.IOException;
-
-public class ListFilesException extends IOException {
-  public ListFilesException(String message){
-    super(message);
-  }
-}
--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/util/DataTransferObject.java
+++
@@ -1,9 +0,0 @@
-package ca.jrvs.apps.jdbc.util;
-
-/**
- * DTO used in both CustomerDAO and OrderDAO
- */
-public interface DataTransferObject {
-
-  long getId();
-}
